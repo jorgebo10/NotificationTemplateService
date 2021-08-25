46e959b547a268708578f7e41969b432e83e26b8
Jorge Bo <jorge.bo@naranjax.com>
2021-08-25 11:38:33 -0300

    update health check

--- src/main/java/com/manning/application/notification/templates/controllers/NotificationTemplateFormatterController.java
+++ src/main/java/com/manning/application/notification/templates/controllers/NotificationTemplateFormatterController.java
@@ -4,6 +4,7 @@ import com.manning.application.notification.templates.model.NotificationTemplate
 import com.manning.application.notification.templates.model.NotificationTemplateFormatterRes;
 import com.manning.application.notification.templates.services.TemplateFormatterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/api/notification/templates")
+@RequestMapping("/api/notifications/templates")
 @RequiredArgsConstructor
 public class NotificationTemplateFormatterController {
     private final TemplateFormatterService templateFormatterService;
@@ -20,5 +21,10 @@ public class NotificationTemplateFormatterController {
     @PostMapping
     public NotificationTemplateFormatterRes create(@RequestBody @Valid NotificationTemplateFormatterReq notificationTemplateFormatterReq) {
         return templateFormatterService.getNotificationTemplateFormatterRes(notificationTemplateFormatterReq);
+    }
+
+    @GetMapping("/healthcheck")
+    public String healthCheck() {
+        return "UP";
     }
 }
