7c403ad7d678c9d1d61f339ba5b1f45e583bcd8c
Jorge Bo <jorge.bo@naranjax.com>
2021-08-25 12:00:19 -0300

    enable service discovery

--- src/main/java/com/manning/application/notification/templates/Application.java
+++ src/main/java/com/manning/application/notification/templates/Application.java
@@ -2,7 +2,9 @@ package com.manning.application.notification.templates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
